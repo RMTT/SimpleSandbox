1e5c8eecd8f07c84cc5247075a8188c30e0404ee
mt <d.rong@outlook.com>
2019-09-13 21:32:39 +0800

    add test for 'a + b' problem

---
+++ tests/a+b.cpp
@@ -0,0 +1,14 @@
+#include <cstdio>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int a, b;
+    for (int i = 0; i < n; i++) {
+        scanf("%d %d", &a, &b);
+
+        printf("%d\n", a + b);
+    }
+    return 0;
+}
